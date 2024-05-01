4226ac1c5bf970e5b22d20cd09453dd064869ea4
sauravkumar2004 <121476013+sauravkumar2004@users.noreply.github.com>
2024-05-01 15:38:48 +0530

    Add files via upload

---
+++ Dice_combination.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define mod 1000000007
+#define ll long long int
+vector<ll> dp(10000000,-1);
+ll f(int n){
+    if(n==0) return 1;
+    if(dp[n]!=-1) return dp[n];
+    ll sum=0;
+    for(int i=1;i<=6;i++){
+        if(n-i<0) break;
+        sum=( sum% mod+f(n-i)%mod)%mod;
+    }
+    return dp[n]=sum%mod;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<f(n);
+}
